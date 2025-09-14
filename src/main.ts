4ac8bcf27d8a135e135f95255e4ef9b8a97fe54f
nemmtor <kacper0witas@gmail.com>
2025-09-14 16:38:31 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import * as BunContext from '@effect/platform-bun/BunContext';
+import * as BunRuntime from '@effect/platform-bun/BunRuntime';
+import * as Effect from 'effect/Effect';
+import * as Layer from 'effect/Layer';
+import { BarService } from './bar';
+import { FooService } from './foo';
+import { program } from './program';
+
+console.log({ foo: Bun.env.FOO });
+
+const MainLayer = Layer.mergeAll(
+  BunContext.layer,
+  BarService.Default,
+  FooService.Live,
+);
+BunRuntime.runMain(
+  program.pipe(Effect.tap(Effect.log), Effect.provide(MainLayer)),
+);
